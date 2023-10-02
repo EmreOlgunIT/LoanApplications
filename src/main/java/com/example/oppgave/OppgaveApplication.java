870e47a356de103b0cfda2f0810cfc689a411763
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-02 10:35:53 +0200

    Initialized project files

---
+++ src/main/java/com/example/oppgave/OppgaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.oppgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppgaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oppgave/OppgaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oppgave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OppgaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

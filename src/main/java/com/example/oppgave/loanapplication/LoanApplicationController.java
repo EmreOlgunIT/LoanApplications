f4328525c170c64b888a3f1544de3216b4b2d17d
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-07 10:27:09 +0200

    Controller and service layer for Loan Application

---
+++ src/main/java/com/example/oppgave/loanapplication/LoanApplicationController.java
@@ -0,0 +1,29 @@
+package com.example.oppgave.loanapplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/loanapplication")
+public class LoanApplicationController {
+
+    private final LoanApplicationService loanApplicationService;
+
+    @Autowired
+    public LoanApplicationController(LoanApplicationService loanApplicationService) {
+        this.loanApplicationService = loanApplicationService;
+    }
+
+    @CrossOrigin
+    @GetMapping
+    public List<LoanApplication> getLoanApplications(){
+        return loanApplicationService.getLoanApplications();
+    }
+
+
+}
---
+++ src/main/java/com/example/oppgave/loanapplication/LoanApplicationService.java
@@ -0,0 +1,24 @@
+package com.example.oppgave.loanapplication;
+
+import com.example.oppgave.customer.Customer;
+import com.example.oppgave.customer.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LoanApplicationService {
+
+    private final LoanApplicationRepository loanApplicationRepository;
+
+    @Autowired
+    public LoanApplicationService(LoanApplicationRepository loanApplicationRepository) {
+        this.loanApplicationRepository = loanApplicationRepository;
+    }
+
+    public List<LoanApplication> getLoanApplications() {
+        return loanApplicationRepository.findAll();
+    }
+
+}

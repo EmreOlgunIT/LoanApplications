71d5f35d8e823d202fb10695470a50b2189a5105
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-10 21:15:39 +0200

    Added service method for retrieving customer by id

--- src/main/java/com/example/oppgave/customer/CustomerService.java
+++ src/main/java/com/example/oppgave/customer/CustomerService.java
@@ -23,4 +23,10 @@ public class CustomerService {
         return customerRepository.save(customer);
     }
 
+    public Customer getCustomerById(Integer id) {
+        return customerRepository.getReferenceById(id);
+    }
+
+
+
 }

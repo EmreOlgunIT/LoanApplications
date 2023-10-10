fdb9a5815617174199686158bda3caafa465847c
Emre <35729378+EmreOlgunIT@users.noreply.github.com>
2023-10-10 21:15:05 +0200

    Added API endpoint for retrieving customer by id

--- src/main/java/com/example/oppgave/customer/CustomerController.java
+++ src/main/java/com/example/oppgave/customer/CustomerController.java
@@ -1,10 +1,7 @@
 package com.example.oppgave.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,11 @@ public class CustomerController {
     @GetMapping
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
+    }
+
+    @GetMapping(path = "{id}")
+    public Customer getCustomerById(@PathVariable("id")  Integer id){
+        return customerService.getCustomerById(id);
     }
 
 }
